cbe25a289081c87317bb03ba7092bae866947281
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-26 12:17:15 +0200

    Change QGIS profile name

--- TestPlugin/build.py
+++ TestPlugin/build.py
@@ -13,7 +13,7 @@ from qgis_plugin_tools.infrastructure.plugin_maker import PluginMaker
 
 py_files = [fil for fil in glob.glob("**/*.py", recursive=True) if "test/" not in fil]
 locales = ['fi']
-profile = 'default'
+profile = 'testplugin'
 ui_files = list(glob.glob("**/*.ui", recursive=True))
 resources = list(glob.glob("**/*.qrc", recursive=True))
 extra_dirs = ["resources", "logs"]
